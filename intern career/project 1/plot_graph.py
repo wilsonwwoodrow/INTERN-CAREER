fb9681c56b98d602be05b4c8830182393de64248
wilsonwwoodrow <134748155+wilsonwwoodrow@users.noreply.github.com>
2023-12-27 14:18:28 -0800

    Update plot_graph.py

--- intern career/project 1/plot_graph.py
+++ intern career/project 1/plot_graph.py
@@ -4,6 +4,6 @@ formatted_time = current_time.strftime("%H:%M:%S")
 from matplotlib import pyplot as plt
 plt.plot(chart_list)
 plt.title(keyword)
-plt.xlabel()
+plt.xlabel(formatted_time)
 plt.ylabel('RATE')
 plt.show()
